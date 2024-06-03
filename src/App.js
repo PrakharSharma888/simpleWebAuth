736a9af0aec2ea35caaec3e8424526bbb009dbae
PrakharSharma888 <prakharentp2002@gmail.com>
2024-06-03 18:08:39 +0530

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import SimpleWebAuthnBrowser from "@simplewebauthn/browser";
+import {
+  startAuthentication,
+  startRegistration,
+} from "@simplewebauthn/browser";
+import {
+  verifyRegistrationResponse,
+  generateAuthenticationOptions,
+} from "@simplewebauthn/server";
 
 function App() {
+  const generateRegistractionOptions = async () => {
+    // registration
+    // const userAuthenticators = {
+    //   authenticators: [
+    //     {
+    //       credentialID: "AWJO-XvawQ4TqJsab3-L33H24DfITrdqnsCrjtZgpKNyqyIyyutlcEvcjci4Ml9KtZW8Xeo3ePlb9w8MC3PJkjs",
+    //       credentialPublicKey: new Uint8Array([5, 6, 7, 8]).buffer,
+    //       counter: "123456",
+    //     }
+    //   ],
+    // };
+
+    // const excludeCredentials = userAuthenticators.authenticators.map((a) => ({
+    //   id: a.credentialID,
+    //   type: "public-key",
+    // }));
+
+    const options = {
+      challenge: "EzJj_WZDFDj9TlIDVv39_f39",
+      rp: {
+        name: "My WebAuthn Service",
+        id: "localhost",
+      },
+      user: {
+        id: "user123",
+        name: "John Doe",
+        displayName: "John D.",
+      },
+      pubKeyCredParams: [
+        {
+          type: "public-key",
+          alg: -7,
+        },
+      ],
+      timeout: 60000,
+      attestation: "direct",
+      excludeCredentials: [],
+      authenticatorSelection: {
+        authenticatorAttachment: "cross-platform",
+        requireResidentKey: false,
+        userVerification: "preferred",
+      },
+      extensions: {},
+    };
+    const registraction = await startRegistration(options);
+    console.log("Registraction ", registraction);
+
+    // verification
+    const verify = await verifyRegistrationResponse({
+      response: registraction,
+      expectedChallenge: "EzJj_WZDFDj9TlIDVv39_f39",
+      expectedOrigin: "http://localhost:3000",
+      expectedRPID: "localhost",
+    });
+    console.log("Verify? ", verify);
+  };
+
+  const login = async () => {
+    const authOp = await generateAuthenticationOptions({
+      // allowCredentials: {
+      //   id: "EzJj_WZDFDj9TlIDVv39_f39",
+      //   type: "public-key",
+      // },
+      rpID: "localhost",
+      challenge: "EzJj_WZDFDj9TlIDVv39_f39",
+      userVerification: "preferred",
+      timeout: 60 * 1000 * 5,
+    });
+
+    console.log("login", authOp);
+
+    const auth = await startAuthentication(authOp);
+    console.log("LastStep? ",auth);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={generateRegistractionOptions}>
+        <h1>Register Here</h1>
+      </button>
+      <button onClick={login}>
+        <h1>Login Here</h1>
+      </button>
     </div>
   );
 }
